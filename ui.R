6a75bc2b534a92539c0cf913e34fb29aae2ea4cb
Colin Millar <colin.millar@ices.dk>
2019-10-21 12:48:49 +0200

    tidy up UI

--- ui.R
+++ ui.R
@@ -1,55 +1,60 @@
 require(shiny)
 require(rhandsontable)
 
-ui <- fluidPage(
-  
-  
-  titlePanel("Seabass Catch Options Tool by Age"),
-  
-  
-  sidebarLayout(
-    
-    sidebarPanel(
-      
-      
-      radioButtons("AdviceType", label = h4("Choose Catch Advice"),
-                   choices = list("EU MAP Fmsy" = "MSY", "EU MAP Fmsy lower" = 1634), 
-                   inline= TRUE, selected = 1946),
-      
-      
-      radioButtons("TimeStep", label = h4("Choose Time Step"),
-                   choices = list("Annual" = 1, "Monthly" = 12), 
-                   inline= TRUE, selected = 1),
-      br(),
-      
-      h5(helpText("Recreational management measures.")),
-      uiOutput("vx"), # vx is coming from renderUI in server.r
-      #br(),
-      uiOutput("vy"), # vy is coming from renderUI in server.r
-      textOutput("RecF"),
-      br(),
-      h5(helpText("Select allowances for commercial gears.")),
-      rHandsontableOutput('table')
-      
+side_width <- 5
 
+sidebar_panel <-
+  sidebarPanel(
+    width = side_width,
+    radioButtons("AdviceType", 
+                 label = h4("Choose Catch Advice"),
+                 choices = list("EU MAP Fmsy" = "MSY", 
+                                "EU MAP Fmsy lower" = "MSYlow"), 
+                 inline = TRUE, 
+                 selected = "MSYlow"),
 
-      ,width=5   
-    ),
+    radioButtons("TimeStep", 
+                 label = h4("Choose Time Step"),
+                 choices = list("Annual" = 1, "Monthly" = 12), 
+                 inline = TRUE, 
+                 selected = 1),
     
-    
-    mainPanel(
-      
-    
-      tableOutput("values"),
-      
-      plotOutput("catch_plot"),
-      
-      plotOutput("n_plot"),
-      
-      verbatimTextOutput("debug_text")
-      
-    )
-    
+    br(),
+    h5(helpText("Recreational management measures.")),
+
+    # SelectOpenSeason is coming from renderUI in server.r
+    uiOutput("SelectOpenSeason"), 
+
+    # SelectBagLimit is coming from renderUI in server.r
+    uiOutput("SelectBagLimit"), 
+
+    textOutput("RecF"),
+
+    br(),
+    h5(helpText("Select allowances for commercial gears.")),
+
+#    rHandsontableOutput('table')
   )
-  
-)
+
+
+main_panel <-    
+  mainPanel(
+    width = 12 - side_width,
+    #tableOutput("values"),
+    #plotOutput("catch_plot"),
+    #plotOutput("n_plot"),
+    verbatimTextOutput("debug_text"),
+    verbatimTextOutput("debug_text_output")
+  )
+ 
+# user interface
+ui <- 
+  fluidPage(
+    titlePanel("Seabass Catch Options Tool by Age"),
+    sidebarLayout(
+      sidebarPanel = sidebar_panel,
+      mainPanel = main_panel,
+      position = "left"
+    ),
+    title = "Seabass Catch Options Tool" # title in the tab
+  )
